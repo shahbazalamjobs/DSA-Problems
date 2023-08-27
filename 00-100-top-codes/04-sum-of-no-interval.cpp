eb86ee77e0a9d3e6ed083d5dcf3d0504478eca1c
Shahbaz Alam <shahbazalamjobs@gmail.com>
2023-08-27 17:28:24 +0530

    Update 04-sum-of-no-interval.cpp

--- 00-100-top-codes/04-sum-of-no-interval.cpp
+++ 00-100-top-codes/04-sum-of-no-interval.cpp
@@ -1,5 +1,6 @@
 // Find the Sum of Numbers in a Given Interval in C++
 
+// 1. using loop
 #include <iostream>
 using namespace std;
 
@@ -27,6 +28,40 @@ int main()
         sum = findSum(num2, num1, sum);
     }
         
+    cout<< sum;
+    
+    return 0;
+}
+
+// 2. Using recursion
+
+// Find the Sum of Numbers in a Given Interval in C++
+
+#include <iostream>
+using namespace std;
+
+int sumRecursive(int num1, int num2) {
+    if(num1 == num2)    // or  num1 > num2
+        return num1;
+    
+    return num2 + sumRecursive(num1, num2 - 1);
+}
+
+int main()
+{
+
+    int num1, num2;
+    
+    cin>> num1 >> num2;
+    int sum = 0;
+   
+    if(num1 < num2) {
+        sum = sumRecursive(num1, num2);    
+    }
+    else {
+        sum = sumRecursive(num2, num1);
+    }
+    
     cout<< sum;
     
     return 0;
