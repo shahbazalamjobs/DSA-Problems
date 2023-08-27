7f4a67b69a5b91fa3499a8610255b1a3926f1a42
Shahbaz Alam <shahbazalamjobs@gmail.com>
2023-08-27 16:41:37 +0530

    Create 03-Sum-of-n.cpp

---
+++ 00-100-top-codes/03-Sum-of-n.cpp
@@ -0,0 +1,69 @@
+// Find the Sum of N Natural Numbers in C++
+
+// 1. Loop
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 0;
+    cin>> num;
+    
+    // Condition to check if the num odd or even
+    
+    int  sum = 0;
+    
+    for (int i = 1; i <= num; i++) {
+        sum = sum + i;
+    }
+    
+    cout << sum;
+    return 0;
+}
+
+// 2. Formula
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 0;
+    cin>> num;
+    
+    //Condition to check if the num odd or even
+    
+    int sum = 0;
+    
+    sum = num * (num + 1) / 2 ;
+    
+    cout << sum;
+    return 0;
+}
+
+
+// 3. Recursively
+
+#include <iostream>
+using namespace std;
+
+// Recursive function 
+int sumRecursive(int num) {
+    if(num == 0){
+        return num;
+    } 
+    
+    return num + sumRecursive(num - 1); 
+}
+
+int main()
+{
+    int num = 0;
+    cin>> num;
+    
+    //Condition to check if the num odd or even
+    int sum = sumRecursive(num);
+    
+    cout << sum;
+    return 0;
+}
