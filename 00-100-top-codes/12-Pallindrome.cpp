383353187c60416bd135db91c35064d2b680bb35
Shahbaz Alam <shahbazalamjobs@gmail.com>
2023-08-28 18:26:43 +0530

    Update 12-Pallindrome.cpp

--- 00-100-top-codes/12-Pallindrome.cpp
+++ 00-100-top-codes/12-Pallindrome.cpp
@@ -58,3 +58,38 @@ int main()
 
 	return 0;
 }
+
+
+// 3. Recursive way to find reverse of a number: Time Complexity : O(N)
+
+#include<iostream> 
+using namespace std;
+
+
+int getReverse(int num, int rev){
+    if(num == 0)
+        return rev;
+    
+    int rem = num % 10;
+    rev = rev * 10 + rem;
+    
+    return getReverse(num / 10, rev);
+}
+
+
+int main ()
+{
+    int num, reverse = 0;
+    num=123454321;
+    cout <<"\nThe number is: "<<num; 
+    
+    
+    // palindrome if num and reverse are equal
+    if (getReverse(num, reverse) == num)
+        cout << num << " is Palindrome";
+    else
+        cout << num << " is not a Palindrome";
+
+}
+
+// 4. We can also use loop to find reverse
