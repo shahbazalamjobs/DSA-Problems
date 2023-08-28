3b2e6c4056e712f01d2314eaef5b3bd6f6e1e23a
Shahbaz Alam <shahbazalamjobs@gmail.com>
2023-08-28 16:19:06 +0530

    Update 11-Reverse-number.cpp

--- 00-100-top-codes/11-Reverse-number.cpp
+++ 00-100-top-codes/11-Reverse-number.cpp
@@ -1,4 +1,6 @@
+// Write a program to reverse digits of a number
 
+// 1. loop, T.C: O(log10N)
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -18,6 +20,32 @@ int main()
 	int num = 4562;
 	cout << "Reverse of no. is " << reverseDigits(num);
 	getchar();
+	return 0;
+}
+
+// 2. recursion 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int reverseDigits(int num)
+{
+	static int rev_num = 0;
+	static int base_pos = 1;
+	if (num > 0) {
+		reverseDigits(num / 10);
+		rev_num += (num % 10) * base_pos;
+		base_pos *= 10;
+	}
+	return rev_num;
+}
+
+// Driver Code
+int main()
+{
+	int num = 4562;
+	cout << "Reverse of no. is " << reverseDigits(num);
+
 	return 0;
 }
 
