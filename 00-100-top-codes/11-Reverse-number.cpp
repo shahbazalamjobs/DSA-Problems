94b11223ba555957245a5ded0afd0b5716c82085
Shahbaz Alam <shahbazalamjobs@gmail.com>
2023-08-28 16:44:00 +0530

    Update 11-Reverse-number.cpp

--- 00-100-top-codes/11-Reverse-number.cpp
+++ 00-100-top-codes/11-Reverse-number.cpp
@@ -49,3 +49,32 @@ int main()
 	return 0;
 }
 
+
+// 3. using string
+
+// C++ program to reverse a number
+#include <bits/stdc++.h>
+using namespace std;
+
+int reverseDigits(int num)
+{
+	// converting number to string
+	string strin = to_string(num);
+
+	// reversing the string
+	reverse(strin.begin(), strin.end());
+
+	// converting string to integer
+	num = stoi(strin);
+
+	// returning integer
+	return num;
+}
+int main()
+{
+	int num = 4562;
+	cout << "Reverse of no. is " << reverseDigits(num);
+	return 0;
+}
+
+// T.C: O(log10n)
