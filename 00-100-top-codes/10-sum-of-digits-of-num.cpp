e2f6681fae07a90bebbbbf48df1ec6619e03e266
Shahbaz Alam <shahbazalamjobs@gmail.com>
2023-08-28 13:09:12 +0530

    Create 10-sum-of-digits-of-num.cpp

---
+++ 00-100-top-codes/10-sum-of-digits-of-num.cpp
@@ -0,0 +1,73 @@
+// Program for Sum of the digits of a given number
+
+// 1.  using loop T.C: O(N)
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int num, sum;
+    
+    cin >> num;
+    
+    sum = 0;
+    
+    while(num != 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    
+    cout << sum;
+    return 0;
+}
+
+// 2. using recursion
+
+#include <iostream>
+using namespace std;
+class gfg {
+public:
+    int sumDigits(int no)
+    {
+        if (no == 0) {
+            return 0;
+        }
+ 
+        return (no % 10) + sumDigits(no / 10);
+    }
+};
+ 
+int main(void)
+{
+    gfg g;
+ 
+    cout << g.sumDigits(687);
+    return 0;
+}
+
+// 3. taking no. as string
+
+// T.C: O(N)
+#include <bits/stdc++.h>
+using namespace std;
+
+int getSum(string str)
+{
+	int sum = 0;
+
+	for (int i = 0; i < str.length(); i++) {
+		// Since ascii value of numbers starts from 48 so we subtract it from sum
+		sum = sum + str[i] - 48;
+	}
+	return sum;
+}
+
+int main()
+{
+	string st = "123456789123456789123422";
+
+	cout << getSum(st);
+	return 0;
+}
+
