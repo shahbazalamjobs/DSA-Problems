de8d643e815057fb0a03ef04d27dcf9b3e9d021e
Shahbaz Alam <shahbazalamjobs@gmail.com>
2023-08-29 18:10:04 +0530

    Update 22-Perfect-No.cpp

--- 00-100-top-codes/22-Perfect-No.cpp
+++ 00-100-top-codes/22-Perfect-No.cpp
@@ -27,3 +27,44 @@ int main ()
 }
 // Time complexity: O(N)
 // Space complexity: O(1)
+
+// 2. Optimized: C++ program to check if a given number is perfect or not O(sqrt(n))
+
+#include<iostream>
+using namespace std;
+
+bool isPerfect(long long int n)
+{
+	
+	long long int sum = 1;
+
+	// Find all divisors and add them
+	for (long long int i=2; i*i<=n; i++)
+	{
+		if (n%i==0)
+		{
+			if(i*i!=n)
+				sum = sum + i + n/i;
+			else
+				sum=sum+i;
+		}
+	}
+	
+	if (sum == n && n != 1)
+		return true;
+
+	return false;
+}
+
+int main()
+{
+	cout << "Below are all perfect numbers till 10000\n";
+	for (int n =2; n<10000; n++)
+		if (isPerfect(n))
+			cout << n << " is a perfect number\n";
+
+	return 0;
+}
+
+
+
