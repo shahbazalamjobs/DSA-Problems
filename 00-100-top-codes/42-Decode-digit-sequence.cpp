330634fba32657625d3eabe984eef8972258157f
Shahbaz Alam <shahbazalamjobs@gmail.com>
2023-09-01 18:01:44 +0530

    Create 42-Decode-digit-sequence.cpp

---
+++ 00-100-top-codes/42-Decode-digit-sequence.cpp
@@ -0,0 +1,14 @@
+// Count Possible Decodings of a given Digit Sequence OR Count possible decodings of a given digit sequence -
+
+/*
+
+1 = A, 2 = B, 3 = C, . . . . , 26 = Z.
+
+Ex : Sequence = 131
+Possible decoding (1, 3, 1,)    = ACA
+Possible decoding (13, 1)   = MA
+So, the total possible decoding’s of sequence 131 is 2.
+
+*/
+
+-------------------------- complete it -----------------------------------------
