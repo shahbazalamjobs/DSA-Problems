c445b6e43848ddb99c1d9e4575a05935a8756399
Shahbaz Alam <shahbazalamjobs@gmail.com>
2023-11-26 02:19:44 +0530

    Update 46-Count-digit-occurences.cpp

--- 00-100-top-codes/46-Count-digit-occurences.cpp
+++ 00-100-top-codes/46-Count-digit-occurences.cpp
@@ -89,3 +89,61 @@ int main() {
 
     return 0;
 }
+
+
+
+// 3. ---------------------------- Short version --------------------
+
+#include <iostream>
+using namespace std;
+int main() {
+
+    int n = 890190798; 
+    int d = 9; 
+
+    int count=0; 
+
+    while(n) {
+
+        int rem = n%10; 
+        if(rem == d){
+            count++;
+        }
+        n=n/10; 
+    }
+
+    cout<<count;
+
+    return 0;
+
+}
+
+
+// 4. ---------------------------- Short version --------------------
+
+#include <iostream>
+using namespace std;
+
+int count(int n, int d){
+    if(n<=0)
+    return 0;
+    
+    int rem = n%10;
+    
+    if(rem == d){
+        return 1 + count(n/10, d);
+    }
+    
+    return count(n/10, d);
+}
+int main() {
+
+    int n = 890190798; 
+    int d = 9; 
+    
+    int x = count(n, d);
+    cout<<x;
+
+    return 0;
+
+}
