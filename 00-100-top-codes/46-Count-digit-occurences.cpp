021582fbf8d908c4583ba78f134025e99c90c465
Shahbaz Alam <shahbazalamjobs@gmail.com>
2023-11-26 02:16:51 +0530

    Update 46-Count-digit-occurences.cpp

--- 00-100-top-codes/46-Count-digit-occurences.cpp
+++ 00-100-top-codes/46-Count-digit-occurences.cpp
@@ -1,3 +1,7 @@
+// Fnd the number of times a specific digit occurs in a given input
+
+// 1 ----------------------------
+
 #include <iostream>
 #include <string>
 using namespace std;
@@ -17,6 +21,51 @@ int countDigitOccurrences(int number, int digit) {
         if (currentDigit == digit) {
             count++;
         }
+    }
+
+    return count;
+}
+
+int main() {
+    int inputNumber, targetDigit;
+
+    // Input number and digit from the user
+    cout << "Enter a number: ";
+    cin >> inputNumber;
+
+    cout << "Enter the digit to count: ";
+    cin >> targetDigit;
+
+    // Call the function to count digit occurrences
+    int occurrences = countDigitOccurrences(inputNumber, targetDigit);
+
+    // Output the result
+    cout << "The digit " << targetDigit << " occurs " << occurrences << " times in the number " << inputNumber << endl;
+
+    return 0;
+}
+
+
+// 2. -----------------------------------
+
+
+#include <iostream>
+using namespace std;
+
+int countDigitOccurrences(int number, int digit) {
+    int count = 0;
+
+    // Iterate through each digit of the number
+    while (number != 0) {
+        int currentDigit = number % 10;
+
+        // Check if the current digit is equal to the target digit
+        if (currentDigit == digit) {
+            count++;
+        }
+
+        // Remove the last digit from the number
+        number /= 10;
     }
 
     return count;
