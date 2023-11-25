6336092d7a0ef33f85ee2c9a97e3c0afd6dd6a40
Shahbaz Alam <shahbazalamjobs@gmail.com>
2023-11-26 01:47:00 +0530

    Update 43-Total-digits-integer.cpp

--- 00-100-top-codes/43-Total-digits-integer.cpp
+++ 00-100-top-codes/43-Total-digits-integer.cpp
@@ -1,3 +1,5 @@
+// 1. ----------------- Simple log formula -------------------
+
 #include <iostream>
 #include <cmath> // for log10
 using namespace std;
@@ -16,6 +18,35 @@ int main() {
     }
 
     cout << "Number of digits: " << digitCount << endl;
+
+    return 0;
+}
+
+
+// 2. ----------------  using while loop ------------------
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long number;
+    int count = 0;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    // Handle negative numbers
+    if (number < 0) {
+        number = -number;
+    }
+
+    // Count the digits using a while loop
+    while (number != 0) {
+        number /= 10;
+        count++;
+    }
+
+    cout << "Number of digits: " << count << endl;
 
     return 0;
 }
